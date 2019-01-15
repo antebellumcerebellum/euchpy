3f55e175292cf4703f61a6064a6cf544f13286f7
Jeremy Marquardt <jsmgob@gmail.com>
2019-01-15 17:56:00 -0500

    Added bCard and rewrote player methods
    bCards a cool dude and you should let him in your game, Adam.

--- misc.py
+++ misc.py
@@ -12,6 +12,37 @@ class card:
 				self.color = 'black'
 
 
+class bCard:
+        #bCard for better card.
+        #like card, but with more variables to work with
+        def __init__(self, suit, rank):
+                self.suit = suit #'c', 's', 'h', 'd'
+                if suit == 'c' or suit == 's':
+                        self.family = 'b'
+                else:
+                        self.family = 'r'
+                self.rank = rank #9-14, 11-J, 12-Q, 13-K, 14-A
+                self.trump = False
+
+        #determines the score of the card
+        #trump - suit of trump
+        #trumpFamily - family of suit that trump belongs to
+        #all cards should have this method run once the kitty card has been revealed.
+        #should be rerun if everyone disses the kitty card.
+        def score(self, trump, trumpFamily):
+                self.score = self.rank
+                if trump == self.suit:
+                        self.trump = True
+                        if 9>=self.rank<=10:
+                                self.score+=6
+                        else:
+                                self.score+=7
+                elif trumpFamily == self.family and self.rank == 11:
+                        self.score+=6
+
+        #to sort in player's hand, use hand.sort(key = lambda x: x.score, reverse=True)
+                        
+
 class kitty:
 	def __init__(self):
 		self.cards = []
--- player.py
+++ player.py
@@ -31,7 +31,7 @@ class player:
 		
 		if self.desiredTrump == topOfKitty.suit and call:
 			return True
-		elif (iter == 2 and call is True):
+		elif (iter == 2 and call is True):#I will never let you live this down - Jeremy
 			return True
 		elif (iter ==2 and self.dealer):
 			return True
@@ -45,6 +45,76 @@ class player:
 				lowest = card
 		print("Lowest card is %d of %s" %(lowest.value, lowest.suit)) 
 		return lowest
+
+       #with bCard
+"""
+        def callTrump(self, topOfKitty, iteration):
+                self.sortHand()
+
+                #if there are more than 2 trump (or 2nd trump jack) in hand, then call
+                if len(list(filter(lambda x: x.score>14, self.hand))) > 2:
+                        return True
+                if iteration == 2 and self.dealer:
+                        return True
+                return False                       
+       
+        def sortHand(self):
+                #to be run before each round, each time a card is added
+                self.hand.sort(key = lambda x: x.score, reverse=True)
+
+        def lowestCard(self):
+                return hand[-1]
+
+        def highestOffSuit(self):
+                trumpless = list(filter(lambda x: not(x.trump), self.hand))
+                return trumpless[0]
+
+        def highestCard(self):
+                return hand[0]
+
+        def playCard(self, card):
+                try:
+                        self.hand.remove(card)
+                except:
+                        print("tried to remove card tht was not in hand")
+                return card
+
+        #I didn't feel like reading throughout the whole playCard method because I am
+        #lazy, so here's a algorithm I didn't even write down first to heck if it made sense
+        def turn(self, cardsPlayed, deck):#cardsPlayed is cards played for THIS round only, deck is list of bCards remaining in deck
+                order = len(cardsPlayed)
+                sortedCardsPlayed = cardsPlayed[:]
+                sortedCardsPlayed.sort(key = lambda x: x.score, reverse = True)
+
+                while True:
+                        #first, see if we can play anything
+                        playable = list(filter(lambda x: x.suit == cardsPlayed[0].suit, self.hand))
+                        #add the trumps
+                        #(I forget if trumplet jacks count, so I'm putting them in anyway)
+                        playable = playable + list(filter(lambda x: not(x in playable) and (x.score>14), self.hand))
+                        #sort it for good measure
+                        playable.sort(key = lambda x: x.score, reverse = True)
+                        #is something can't be played, draw and try again
+                        if len(playable) == 0:
+                                if len(deck) == 0:
+                                        print("Fuck, I forgot what happens in this case")
+                                        continue
+                                self.Hand.append(deck.pop())#remove card from deck, add it to hand
+                                continue
+                        if playable[0].score < sortedCardsPlayed[0].score:
+                                #if the cards out so far are all too good, there's no point in wasting a good card. Play a bad card
+                                return self.playCard(playable[-1])
+                        if playable[0].score >= sortedCardsPlayed[0].score:
+                                if order >= 2 and sortedCardsPlayed[0] == cardsPlayed[-2]:#if your partner is winning don't sabotage
+                                        return self.playCard(playable[-1])
+                                #else, it could be worth playing something higher.
+                                #it could be worth playing higher, but the level of conservativeness you should play with should be weighted by how many more people have to take their turns, and how many cards you have that are high
+                                neoPlayable = list(filter(lambda x: x.score > sortedCardsPlayed[0].score))
+                                #numNeoPlay = len(neoPlayable)
+                                #conservatism = 4 - order
+                                #if numNeoPlay 
+                                return self.playCard(neoPlayable[0])# eh, screw the math. Play defensively.
+"""
     
 	def highestOffSuit(self):
 		highest = self.hand[0]
